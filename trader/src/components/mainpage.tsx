aab4bf3172e1c160f4eedb582092820c24cbc38b
appcoreopc <jeremy@jeremys-MacBook-Pro.local>
2021-05-23 13:25:44 +0800

    added components

---
+++ trader/src/components/mainpage.tsx
@@ -0,0 +1,32 @@
+import Grid from '@material-ui/core/Grid';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import { NavigationBar } from './trader/navigationBar';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }),
+);
+
+export const MainPage = (props:any) => {
+
+  const classes = useStyles();  
+    return (
+      <div className={classes.root}>
+        <NavigationBar />
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+              hello there! I am using a grid material ui. 
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
