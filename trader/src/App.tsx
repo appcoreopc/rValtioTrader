1db8d8eb3850b743aa69b5f51a41a939630627b8
appcoreopc <jeremy@jeremys-MacBook-Pro.local>
2021-05-29 09:29:00 +0800

    added redux integration

--- trader/src/App.tsx
+++ trader/src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 import { MainPage } from './components/mainpage';
+import store from './store/store';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
     <div className="App">
-      
-      <MainPage>
-      </MainPage>
+      <Provider store={store}>
+        <MainPage>
+        </MainPage>
+      </Provider>
     </div>
   );
 }
--- trader/src/components/toolbar/saveButton.tsx
+++ trader/src/components/toolbar/saveButton.tsx
@@ -2,11 +2,16 @@ import React, { useState, Suspense, lazy } from 'react';
 import SaveIcon from '@material-ui/icons/Save';
 import { useStyles } from './style';
 import Button from '@material-ui/core/Button';
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from '../../store/toolbarSlicer';
 
 export const SaveButton = () => {
 
     const classes = useStyles();
     const [enabled, setActive] = useState(false);
+    
+    const count = useSelector((state) => state);
+    const dispatch = useDispatch();
 
     return (
       <>
@@ -14,7 +19,8 @@ export const SaveButton = () => {
         variant="contained"
         color="primary"
         size="small"
-        className={classes.menuButton} onClick={() => { alert('clicked') }}
+        className={classes.menuButton} 
+        onClick={() => dispatch(decrement())}
         startIcon={<SaveIcon />}>Save</Button>
       </>
     );
