b5d49e59965da540e036bffcbd1fe9893ca735df
appcoreopc <jeremy@jeremys-MacBook-Pro.local>
2021-06-03 15:02:31 +0800

    added drawer

--- trader/src/components/mainpage.tsx
+++ trader/src/components/mainpage.tsx
@@ -4,39 +4,42 @@ import { NavigationBar } from './trader/navigationBar';
 import { BottomBar } from './trader/bottomNav';
 import { Page } from '../themes/countContext';
 import { ErrorBoundary } from "./errorBoundary/errorBoundary";
+import { ResponsiveDrawer } from "./trader/appdevToolbar";
 
 const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-  }),
+createStyles({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}),
 );
 
 export const MainPage = (props:any) => {
-
+  
   const classes = useStyles();  
-    return (
-      <div className={classes.root}>
-        <ErrorBoundary>
-
-        <NavigationBar />
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-              <Page />
-          </Grid>
-        </Grid>
-
-        <BottomBar></BottomBar>
-
-        </ErrorBoundary>
-      
-      </div>
+  return (
+    <div className={classes.root}>
+    <ErrorBoundary>
+    
+    <NavigationBar />
+    
+    <ResponsiveDrawer/>
+    <Grid container spacing={3}>
+    <Grid item xs={12}>
+    <Page />
+    </Grid>
+    </Grid>
+    
+    <BottomBar></BottomBar>
+    
+    </ErrorBoundary>
+    
+    </div>
     );
   }
